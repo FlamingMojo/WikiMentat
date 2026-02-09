e3042ef94d64346cbc53de293ef921f2ada92df1
FlamingMojo <mojo@flamingmojo.dev>
2026-02-09 08:46:44 +0000

    Fix: Docker build process

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,4 +1,8 @@
-Discord::Bot.setup
+unless ENV['SKIP_DISCORD_BOT'] == 'true'
+  # Setup still calls off to Discord and requires tokens etc. Sometimes (e.g. Docker build process)
+  # we don't want that at all, so just skip it.
+  Discord::Bot.setup
+end
 
 if ENV['RUN_DISCORD_BOT'] == 'true'
   Discord::Bot.run(true)
