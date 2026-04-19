48415f29b24d0a2bf00dc404d2dfdfa0d41c82db
FlamingMojo <mojo@flamingmojo.dev>
2026-04-19 17:12:36 +0100

    Refactor Approve and Reject logic;
    - Makes it easier to have these in their own subclasses so Missions can fully control that workflow

--- app/bots/discord/commands/missions/approve.rb
+++ app/bots/discord/commands/missions/approve.rb
@@ -18,62 +18,11 @@ module Discord::Commands::Missions
       return t('not_assigned') unless assignee
 
       mission.approve
-      Discord.send_message(
-        channel: notifications_channel.discord_uid,
-        content: t('celebration', user: assignee.discord_uid, summary: mission.summary, count: mission_count)
-      )
-      handle_reward
-
-      t('approved_mission', summary: mission.summary)
     rescue => error
       DiscordError.handle(error:, user: mentat_user, service: self.class.to_s)
     end
 
     private
-
-    def handle_reward
-      return unless guild_config.enable_rewards
-      return unless reward_types.map(&:threshold).include?(mission_count)
-      return out_of_stock unless reward_type_due.in_stock?
-
-      member_reward
-      Discord.send_message(
-        channel: admin_channel.discord_uid,
-        content: t('reward', user: assignee.discord_uid, count: mission_count, reward: reward_type_due.name),
-        components: confirm_button,
-      )
-    end
-
-    def reward_types
-      @reward_types ||= guild_config.reward_types.mission_count
-    end
-
-    def reward_type_due
-      @reward_type_due ||= reward_types.find_by(threshold: mission_count)
-    end
-
-    def out_of_stock
-      Discord.send_message(
-        channel: admin_channel.discord_uid,
-        content: t('reward_out_of_stock', user: assignee.discord_uid, reward: reward_type_due.name)
-      )
-    end
-
-    def member_reward
-      @member_reward ||= reward_type_due.next_reward.issue_to(assignee)
-    end
-
-    def confirm_button
-      ::Discordrb::Components::View.new do |builder|
-        builder.row do |row|
-          row.button(label: t('confirm'), custom_id: "mission:reward:confirm:#{member_reward.id}", style: :success)
-        end
-      end
-    end
-
-    def mission_count
-      @mission_count ||= assignee.reload.missions.completed.count
-    end
 
     def assignee
       @assignee ||= mission.assignee
@@ -81,18 +30,6 @@ module Discord::Commands::Missions
 
     def mission
       @mission ||= guild_config.missions.find_by(id: custom_id.split(':').last)
-    end
-
-    def admin_channel
-      @admin_channel ||= guild_config.mission_admin_channel
-    end
-
-    def notifications_channel
-      @notifications_channel ||= guild_config.mission_notifications_channel
-    end
-
-    def guild_config
-      @guild_config ||= guild.primary_config
     end
   end
 end
--- app/bots/discord/commands/missions/reject.rb
+++ app/bots/discord/commands/missions/reject.rb
@@ -16,38 +16,13 @@ module Discord::Commands::Missions
       return t('not_submitted') unless mission.submitted?
       return t('not_assigned') unless assignee
 
-      send_pm(user_id: assignee.discord_uid.to_i, message: t('feedback', summary: mission.summary))
-      # Catch the edge case where a user has picked up another mission while awaiting this being accepted.
-      return abandon_mission if assignee.missions.accepted.count >= 1
       mission.reject
-
-      t('rejected_mission', summary: mission.summary)
     rescue => error
       DiscordError.handle(error:, user: mentat_user, service: self.class.to_s)
       t('rejected_mission_no_feedback', summary: mission.summary)
     end
 
     private
-
-    def abandon_mission
-      mission.abandon
-      send_pm(user_id: assignee.discord_uid.to_i, message: abandon_pm_message)
-
-      t('rejected_abandoned_mission', summary: mission.summary)
-    end
-
-    def abandon_pm_message
-      t(
-        'abandon',
-        current_mission: assignee.current_mission.summary,
-        summary: mission.summary,
-        link: mission.reload.discord_post_link
-      )
-    end
-
-    def assignee
-      @assignee ||= mission.assignee
-    end
 
     def mission
       @mission ||= Mission.find_by(id: custom_id.split(':').last)
