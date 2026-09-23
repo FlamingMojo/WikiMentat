f3bf744072712ef797e8b519430d9f4b67b82cad
FlamingMojo <mojo@flamingmojo.dev>
2026-09-23 08:47:16 +0100

    Add manual submit button for missions;
    - Sometimes they get stuck. Just allow folks to manually submit their own missions

--- app/bots/discord/commands/missions/submit.rb
+++ app/bots/discord/commands/missions/submit.rb
@@ -1,17 +1,26 @@
 # frozen_string_literal: true
 
-# Invoked from automated submit actions (non command)
-# Updates mission status to submitted
-# Updates mission embed with new status
-# Posts submit embed with mission details plus submission details, and buttons
+# Invoked from manual submit button on mission cards
 module Discord::Commands::Missions
   class Submit
     include ::Translatable
     include ::Discord::Util
 
-    with_locale_context 'discord.commands.missions'
+    with_locale_context 'discord.commands.missions.submit'
 
     def content
+      return t('not_found') unless mission
+      return t('not_yours') if mission.assignee != mentat_member
+
+      mission.submit
+
+      t('manual_submit', summary: mission.summary)
+    end
+
+    private
+
+    def mission
+      @mission ||= Mission.find_by(id: custom_id.split(':').last)
     end
   end
 end
